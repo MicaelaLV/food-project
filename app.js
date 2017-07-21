a368233f7d1f43e7f9db3b7921a6e98207b46af3
Micaela Linda Villani <villani.micaela@gmail.com>
2017-07-21 14:05:26 +0200

    fix error in dotenv quotes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //heroku mongo dbs requires:
-require(“dotenv”).config();
+require("dotenv").config();
 
 const express                 = require("express");
 const path                    = require("path");
